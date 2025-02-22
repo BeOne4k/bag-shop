5bdaa9edd57ed1622d1ec3cfd57136de9e7cb68d
BeOne4k <beoone555@gmail.com>
2025-02-22 15:08:02 +0400

    nav responsive for 800px done

--- script/main.js
+++ script/main.js
@@ -1,2 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger-menu");
+    const navMenu = document.querySelector(".resp-nav");
 
-
+    burger.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+        burger.classList.toggle("active");
+    });
+});
